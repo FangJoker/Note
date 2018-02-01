dc711967ee0268fe4633f3740370d2f4665219c6
FangJoker <598592454@qq.com>
2018-02-01 23:32:52 +0800

    文件上传控制器

--- MySpringMVC/src/com/czl/controller/FileController.java
+++ MySpringMVC/src/com/czl/controller/FileController.java
@@ -2,17 +2,22 @@ package com.czl.controller;
 
 import java.io.*;
 import java.util.Date;
+import java.util.Iterator;
 import java.text.SimpleDateFormat;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.multipart.*;
+import org.springframework.web.multipart.*; //文件上传包
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 @Controller
 @RequestMapping("/File")
@@ -47,4 +52,23 @@ public class FileController {
 		return "/upload";	
 	}
 
+	@RequestMapping(value ="/upload2" ,method=RequestMethod.POST)
+	public ModelAndView Upload2(HttpServletRequest request, HttpServletResponse response) throws IllegalStateException, IOException{
+		CommonsMultipartResolver multipartResolver  = new CommonsMultipartResolver(request.getSession().getServletContext());  //加载文件解析器，传入配置信息
+		if(multipartResolver.isMultipart(request)){  //判断request里是否有文件
+			MultipartHttpServletRequest  multiRequest = (MultipartHttpServletRequest)request; //将request转换为MultipartHttpServletRequest
+			Iterator<String>  iter = multiRequest.getFileNames(); //启用迭代器获取文件名
+			 while(iter.hasNext()){   //遍历文件
+				 MultipartFile file = multiRequest.getFile((String)iter.next());  //取得文件
+				  if(file != null){
+					  String fileName = "Upload" + file.getOriginalFilename(); //文件名
+					  String path = "C:/Users/59859/Workspaces/MyEclipse 2016 CI/MySpringMVC/WebRoot/images/" + fileName;      //储存路径
+					  File localFile = new File(path);
+					  file.transferTo(localFile);    //利用SpringMVC自带的方法上传文件到本地
+				  }
+			 }  
+		}
+		return new ModelAndView("/success", "file","文件上传成功！");
+	}
+	
 }
